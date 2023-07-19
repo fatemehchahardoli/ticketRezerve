0f753057992a528ab297b9d2c661745c296b7655
fatemehchahardoli <chahardolifatemehb@gmail.com>
2023-07-19 18:07:34 +0330

    change book tick

--- src/components/BookingConcertTicket.js
+++ src/components/BookingConcertTicket.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import SectionA from "./SectionA";
 import "../style/main.scss";
-import SectionB from "./SectionB";
-import SectionC from "./SectionC";
-import SectionD from "./SectionD";
+import Section from "./Section";
 export const dataChairsContext = React.createContext();
+
 const BookingConcertTicket = () => {
-   const initDataState = [];
-   const [data, setData] = useState(initDataState);
+   const [data, setData] = useState([]);
+   const [count, setCount] = useState(0);
+   const [sum, setSum] = useState(0);
+   const [sections, setSections] = useState(["B", "A", "C", "D"]);
+
    useEffect(() => {
-      axios.get("/chairs.json").then((Response) => setData(Response.data));
+      axios.get("/chairs.json").then((response) => setData(response.data));
    }, []);
 
    return (
@@ -20,26 +21,19 @@ const BookingConcertTicket = () => {
                <h1>stage</h1>
             </div>
             <div className="factor">
-               <h3>CounterChair:</h3> <div className="counter">0</div>
-               <h3>TotalPrice:</h3> <div className="total-Price">0</div>
+               <h3>CounterChair:</h3> <div className="counter">{count}</div>
+               <h3>TotalPrice:</h3> <div className="total-Price">{sum}</div>
             </div>
             <div className="chairs-grid">
-               {/* sectionB */}
-               <dataChairsContext.Provider value={data}>
-                  <SectionB></SectionB>
-               </dataChairsContext.Provider>
-               {/* sectionA */}
-               <dataChairsContext.Provider value={data}>
-                  <SectionA></SectionA>
-               </dataChairsContext.Provider>
-               {/* sectionC */}
-               <dataChairsContext.Provider value={data}>
-                  <SectionC></SectionC>
-               </dataChairsContext.Provider>
-               {/* sectionD */}
-               <dataChairsContext.Provider value={data}>
-                  <SectionD></SectionD>
-               </dataChairsContext.Provider>
+               {sections.map((section, index) => (
+                  <dataChairsContext.Provider value={data} key={index}>
+                     <Section
+                        section={section}
+                        setCount={setCount}
+                        setSum={setSum}
+                     ></Section>
+                  </dataChairsContext.Provider>
+               ))}
             </div>
          </div>
       </>
